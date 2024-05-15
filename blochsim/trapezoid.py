32492e8ddbb4928da696f39045a8043db28ea9d2
Kwang Eun Jang <kejang@stanford.edu>
2024-05-14 18:34:57 -0700

    writing trapezoid.py

--- blochsim/trapezoid.py
+++ blochsim/trapezoid.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 
 def round_up_length(x, round_n=2):
@@ -7,213 +8,325 @@ def round_up_length(x, round_n=2):
     return round_n * int(np.ceil(x / round_n))
 
 
-def get_trap(amp, ramp, plateau, boundary='right', dtype='float32'):
+def get_nrmp(amp, smax, dt):
+    """Returns the number of points of the ramp.
+
+    Args:
+        amp (float): target gradient amplitude in G/cm
+        smax (float): slew rate in G/cm/ms
+        dt (float | int): sampling interval in us
+
+    Returns:
+        int: number of points of the ramp.
+    """
+    return int(np.ceil(np.abs(amp) / dt / smax * 1000 - 1))
+
+
+def get_trap_area(amp, nrmp, nplt, dt):
+    """Returns the area of the trapezoid.
+
+    Args:
+        amp (float): amplitude of the plateau
+        nrmp (it): number of points of the ramp
+        nplt (int): number of points of the plateau
+        dt (float | int): sampling interval in us
+
+    Returns:
+        _type_: _description_
+    """
+
+    # d = np.abs(amp) / (nrmp + 1)
+    # area_ramp = (1 + nrmp) * nrmp * d
+    # area_plateau = nplt * amp
+    # return dt * (area_ramp + area_plateau)
+
+    return dt * amp * (nrmp + nplt)
+
+
+def get_trap(amp, nrmp, nplt, dtype="float32"):
     """Returns a trapezoid.
 
     Args:
-        amp (float): amplitude in G/cm
-        ramp (int): length of ramp
-        plateau (int): length of plateau
-        boundary (str): {'right', 'left', 'both', 'neither'}
-        dtype (str): dtype of ndarray
+        amp (float): amplitude of the plateau in G/cm
+        nrmp (int): number of points of the ramp
+        nplt (int): number of points of the plateau
+        dtype (str, optional): dtype of ndarray. Defaults to "float32".
 
     Returns:
-        `ndarray`: trapezoid
+        ndarray: trapezoid
+
+    Notes:
+        The previous gradient amplitude is assumed to be zero. The end of this
+        trapezoid is zero.
+        (In other words, it follows "right-side" boundary convention.)
     """
 
-    if boundary == 'right':
-        attack = np.linspace(0, amp, ramp + 1, endpoint=True)[1:]
-        decay = np.linspace(0, amp, ramp, endpoint=False)[::-1]
-    elif boundary == 'left':
-        attack = np.linspace(0, amp, ramp, endpoint=False)
-        decay = (np.linspace(0, amp, ramp + 1, endpoint=True)[1:])[::-1]
-    elif boundary == 'both':
-        attack = np.linspace(0, amp, ramp, endpoint=False)
-        decay = attack[::-1]
-    else:    # neither
-        attack = np.linspace(0, amp, ramp + 1, endpoint=False)[1:]
-        decay = attack[::-1]
+    trap = np.zeros((2 * nrmp + nplt + 1), dtype=dtype)
+    d = amp / (nrmp + 1)
 
-    trap = np.concatenate(
-        [attack,
-         amp * np.ones(plateau),
-         decay]
-    )
+    for i in range(nrmp):
+        trap[i] = (i + 1) * d
 
-    return trap.astype(dtype)
+    for i in range(nrmp, nrmp + nplt):
+        trap[i] = amp
+
+    for i in range(nrmp + nplt, 2 * nrmp + nplt):
+        trap[i] = amp - (i - nrmp - nplt + 1) * d
+
+    return trap
 
 
-def get_trap_slice_select(
-    slthick,
-    dur,
-    bw,
-    gmax=4.0,
-    smax=15.0,
-    interval=4,
-    boundary='right',
-    dtype='float32',
-    gamma=4257.59,
-    round_n=1,
-):
-    """Returns a slice-select trapezoid.
-
-    Args:
-        slthick (float): slice thickness in cm
-        dur (int): RF duration in us
-        bw (float): RF bandwidth in Hz
-        gmax (float): maximum gradient amplitude in G/cm
-        smax (float): maximum gradient slew rate in G/cm/ms
-        interval (int): time interval in us
-        boundary (str): {'right', 'left', 'both'}
-        dtype (str): dtype of ndarray
-        gamma (float): gyromagnetic ratio in Hz/G
-        round_n (int): round up ramp length to multiple of this number
-
-    Returns:
-        tuple: `ndarray` (trapezoid) and int (length of ramp)
-    """
-
-    amp = bw / (slthick * gamma)
-
-    if amp > gmax:
-        # bandwidth is too large, and/or slice thickness is too thin.
-        return None, None
-
-    smax *= 1e-3    # G/cm/us
-
-    ramp = round_up_length(amp / smax / interval, round_n)
-    plateau = round_up_length(dur / interval, round_n)
-    trap = get_trap(amp, ramp, plateau, boundary, dtype)
-
-    return trap, ramp
-
-
-def get_trap_given_area(
+def design_trap_given_area(
     area,
     min_plateau=2,
-    gmax=4.0,
-    smax=15.0,
-    interval=4,
-    boundary='right',
-    dtype='float32',
-    round_n=1,
+    gmax=3.9,
+    smax=14.9,
+    dt=4,
 ):
-    """Returns a trapezoid with given area.
+    """Returns trapezoid parameters for the given area.
 
     Args:
-        area (float): area of trapezoid in G/cm * us
-        min_plateau (int): minimum plateau length
-        gmax (float): maximum gradient amplitude in G/cm
-        smax (float): maximum gradient slew rate in G/cm/ms
-        interval (int): time interval in us
-        boundary (str): {'right', 'left', 'both', 'neither'}
-        dtype (str): dtype of ndarray
-        round_n (int): round up ramp length to multiple of this number
+        area (float): desired area in G/cm * us
+        min_plateau (int, optional): minimum number of points of the plateau. Defaults to 2.
+        gmax (float, optional): maximum gradient amplitude in G/cm. Defaults to 3.9.
+        smax (float, optional): maximum slew-rate in G/cm/ms. Defaults to 14.9.
+        dt (int | float, optional): sampling interval in us. Defaults to 4.
 
     Returns:
-        tuple: `ndarray` (trapezoid) and int (length of ramp)
+        tuple: (amp, nrmp, nplt)
     """
 
-    smax *= 1e-3    # G/cm/us
-    min_ramp = round_up_length(gmax / smax / interval, round_n)
-    tt = np.abs(area) / gmax - min_ramp * interval
+    # first, design the trapezoid using the analytic model
 
-    if tt > min_plateau * interval:
-        plateau = round_up_length(tt / interval, round_n)
-        amp = gmax
-        ramp = min_ramp
+    nrmp = get_nrmp(gmax, smax, dt)
+    nplt = int(np.ceil(np.abs(area) / dt / gmax - nrmp))
+
+    if nplt < min_plateau:
+        nplt = min_plateau
+        b = (nplt - 1) * dt * (smax / 1000.0)
+        amp = (-b + np.sqrt(b**2 + 4 * (smax / 1000.0) * np.abs(area))) / 2
+        nrmp = get_nrmp(amp, smax, dt)
     else:
-        plateau = min_plateau
-        tt = plateau * interval
-        amp = (-tt * smax
-               + np.sqrt((tt * smax)**2 + 4 * smax * np.abs(area))) / 2
-        ramp = round_up_length(amp / smax / interval, round_n)
+        nplt = 1 + int(np.ceil(np.abs(area) / gmax / dt - gmax / (smax / 1000.0) / dt))
+        nrmp = get_nrmp(gmax, smax, dt)
+        if area > 0:
+            amp = gmax
+        else:
+            amp = -gmax
 
-    trap = get_trap(amp, ramp, plateau, boundary, dtype)
-    trap *= area / (np.sum(trap) * interval)
+    # compensate the error of the analytic model
 
-    return trap, ramp
+    area_temp = get_trap_area(amp, nrmp, nplt, dt)
+    amp *= area / area_temp
+
+    return amp, nrmp, nplt
+
+
+def cart_to_sph(x, y, z):
+    """
+    Convert Cartesian to Spherical coordinate.
+
+    Args:
+        x (float): x-coordinate in Cartesian.
+        y (float): y-coordinate in Cartesian.
+        z (float): z-coordinate in Cartesian.
+
+    Returns:
+        tuple: A tuple containing the following spherical coordinates:
+            rho (float): Radial distance from the origin.
+            azimuthal_angle (float): Azimuthal angle.
+            polar_angle (float): Polar angle.
+    """
+    r = np.sqrt(x**2 + y**2 + z**2)
+    azimuthal_angle = np.arctan2(y, x)
+    polar_angle = np.arccos(z / r)
+
+    return r, azimuthal_angle, polar_angle
+
+
+def design_concurrent_trap(
+    area_x, area_y, area_z, min_plateau=2, gmax=3.9, smax=14.9, dt=4
+):
+    """Returns concurrent trapezoid parameters for the given area.
+
+    Args:
+        area_x (float): desired area on XGRAD in G/cm * us
+        area_y (float): desired area on YGRAD in G/cm * us
+        area_z (float): desired area on ZGRAD in G/cm * us
+        min_plateau (int, optional): minimum number of points of the plateau. Defaults to 2.
+        gmax (float, optional): maximum gradient amplitude in G/cm. Defaults to 3.9.
+        smax (float, optional): maximum slew-rate in G/cm/ms. Defaults to 14.9.
+        dt (int | float, optional): sampling interval in us. Defaults to 4.
+
+    Returns:
+        tuple: (amp_x, amp_y, amp_z, nrmp, nplt)
+    """
+
+    rho, theta, phi = cart_to_sph(area_x, area_y, area_z)
+
+    amp, nrmp, nplt = design_trap_given_area(rho, min_plateau, gmax, smax, dt)
+
+    rotmat = Rotation.from_euler("yz", [phi, theta]).as_matrix()
+    amps = np.asarray(np.matmul(rotmat, [0, 0, amp]))
+    amp_x, amp_y, amp_z = amps
+
+    return amp_x, amp_y, amp_z, nrmp, nplt
+
+
+# def get_trap_slice_select_composite(
+#     slthick,
+#     rf_dur,
+#     bw,
+#     include_dep=False,
+#     include_rep=False,
+#     gmax=4.0,
+#     smax=14.9,
+#     dt=4,
+#     min_plateau=2,
+#     gamma=4257.58,
+#     round_n=4,
+#     dtype="float32",
+# ):
+#     """Returns a composite gradient waveform for slice-selection.
+
+#     Args:
+#         slthick (float): slice thickness in cm
+#         rf_dur (float): rf duration in ms
+#         bw (float): rf bandwidth in Hz
+#         include_dep (bool, optional): whether to include the dephaser. Defaults to False.
+#         include_rep (bool, optional): whether to include the rephaser. Defaults to False.
+#         gmax (float, optional): maximum gradient amplitude in G/cm. Defaults to 4.0.
+#         smax (float, optional): maximum slew-rate in G/cm/ms. Defaults to 14.9.
+#         dt (int | float, optional): sampling interval in us. Defaults to 4.
+#         min_plateau (int, optional): minimum number of points of the plateau. Defaults to 2.
+#         gamma (float, optional): Gyromagnetic ratio in Hz/G. Defaults to 4257.58.
+#         round_n (int, optional): Rounding factor for the total number of points. Defaults to 4.
+#         dtype (str, optional): dtype of ndarray. Defaults to "float32".
+
+#     Returns:
+#         tuple: (ndarray, int) trapezoid and the number of points before rf.
+#     """
+
+#     amp = bw / (slthick * gamma)
+
+#     npt_rf = int(np.ceil(rf_dur * 1000 / dt))
+#     nrmp = get_nrmp(amp, smax, dt)
+#     trap_ss = get_trap(amp, nrmp, npt_rf, dtype)
+
+#     npt_delay = nrmp
+
+#     if include_dep:
+#         trap_dep, _ = get_trap_given_area(
+#             -0.5 * np.sum(trap_ss) * dt,
+#             min_plateau=min_plateau,
+#             gmax=gmax,
+#             smax=smax,
+#             dt=dt,
+#             dtype=dtype,
+#         )
+#         npt_delay += len(trap_dep)
+#     else:
+#         trap_dep = []
+
+#     if include_rep:
+#         trap_rep, _ = get_trap_given_area(
+#             -0.5 * np.sum(trap_ss) * dt,
+#             min_plateau=min_plateau,
+#             gmax=gmax,
+#             smax=smax,
+#             dt=dt,
+#             dtype=dtype,
+#         )
+#     else:
+#         trap_rep = []
+
+#     trap_temp = np.concatenate([trap_dep, trap_ss, trap_dep])
+
+#     npt_total = round_up_length(len(trap_temp), round_n)
+
+#     trap = np.zeros(npt_total, dtype=dtype)
+#     trap[: len(trap_temp)] = trap_temp
+
+#     return trap, npt_delay
+
+
+def design_trap_largest(
+    n,
+    gmax=3.9,
+    smax=14.9,
+    kmax=1e9,
+    dt=4,
+    min_plateau=2,
+    gamma=4257.58
+):
+
+    nrmp = get_nrmp(gmax, smax, dt)
+    nplt = n - 2 * nrmp - 1
+
+    if nplt > min_plateau:
+        amp = gmax
+    else:
+        nrmp = int((n - min_plateau - 1) / 2)
+        nplt = n - 2 * nrmp - 1
+        amp = (nrmp + 1) * (smax * 1e-3) * dt
+
+    k = 1e-6 * dt * gamma * amp * (nrmp + nplt)
+
+    if k < kmax:
+        return amp, nrmp, nplt
+    else:
+        b = -dt * n * (smax * 1e-3)
+        c = kmax * smax / gamma * 1e3
+
+        amp_0 = 0.5 * (-b - np.sqrt(b ** 2 - 4 * c))
+        amp_1 = 0.5 * (-b + np.sqrt(b ** 2 - 4 * c))
+        if np.abs(amp_0) < gmax:
+            amp = amp_0
+        else:
+            amp = amp_1
+
+        nrmp = get_nrmp(amp, smax, dt)
+        nplt = n - 2 * nrmp - 1
+
+        return amp, nrmp, nplt
 
 
 def get_trap_largest(
     n,
-    gmax=4.0,
-    smax=15.0,
+    gmax=3.9,
+    smax=14.9,
+    kmax=1e9,
+    dt=4,
     min_plateau=2,
-    interval=4,
-    boundary='right',
+    gamma=4257.58,
     dtype='float32',
-    round_n=1,
 ):
-    """Returns the largest trapezoid for given length.
 
-    Args:
-        n (int): length (total number of sampling points) of trapezoid
-        gmax (float): maximum gradient amplitude in G/cm
-        smax (float): maximum gradient slew rate in G/cm/ms
-        min_plateau (int): minimum plateau length
-        interval (int): time interval in us
-        boundary (str): {'right', 'left', 'both'}
-        dtype (str): dtype of ndarray
-        round_n (int): round up ramp length to multiple of this number
+    amp, nrmp, nplt = design_trap_largest(
+        n,
+        gmax,
+        smax,
+        kmax,
+        dt,
+        min_plateau,
+        gamma
+    )
 
-    Returns:
-        tuple: `ndarray` (trapezoid) and int (length of ramp)
-    """
-    min_ramp = round_up_length(gmax / (smax * 1e-3) / interval, round_n)
-
-    max_ramp_raw = (n - min_plateau) / 2
-    max_ramp = round_up_length(max_ramp_raw, round_n)
-
-    while (min_plateau + 2 * max_ramp > n):
-        max_ramp_raw -= 0.5
-        max_ramp = round_up_length(max_ramp_raw, round_n)
-
-    if min_ramp < max_ramp:
-        amp = gmax
-        ramp = min_ramp
-        plateau = n - 2 * ramp
-    else:
-        amp = (smax * 1e-3) * max_ramp * interval
-        ramp = max_ramp
-        plateau = n - 2 * ramp
-
-    return get_trap(amp, ramp, plateau, boundary, dtype), ramp
+    trap = get_trap(amp, nrmp, nplt, dtype)
+    
+    return trap, nrmp
 
 
 def get_trap_triangle(
     amp,
-    gmax=4.0,
-    smax=15.0,
+    smax=14.9,
     min_plateau=2,
-    interval=4,
-    boundary='right',
+    dt=4,
     dtype='float32',
-    round_n=1,
 ):
-    """Returns a triangular trapezoid.
 
-    Args:
-        amp (float): amplitude in G/cm
-        gmax (float): maximum gradient amplitude in G/cm
-        smax (float): maximum gradient slew rate in G/cm/ms
-        min_plateau (int): minimum plateau length
-        interval (int): time interval in us
-        boundary (str): {'right', 'left', 'both'}
-        dtype (str): dtype of ndarray
-        round_n (int): round up ramp length to multiple of this number
+    nrmp = get_nrmp(amp, smax, dt)
+    nplt = min_plateau
+    trap = get_trap(amp, nrmp, nplt, dtype)
 
-    Returns:
-        tuple: `ndarray` (trapezoid) and int (length of ramp)
-    """
-
-    if amp > gmax:
-        return None, None
-
-    smax *= 1e-3    # G/cm/us
-
-    ramp = round_up_length(amp / smax / interval, round_n)
-    plateau = min_plateau
-    trap = get_trap(amp, ramp, plateau, boundary, dtype)
-
-    return trap, ramp
+    return trap, nrmp
